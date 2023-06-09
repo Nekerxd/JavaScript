28748f8f66727fb2f69636b26764085dc9eed7b6
Nekerxd <nuvielxd_01@hotmail.com>
2023-06-09 12:05:22 -0300

    Organização Exercícios

---
+++ Seção 3 - JavaScript Básico/Aula 5 - Constantes com const/exercício/ex04.js
@@ -0,0 +1,13 @@
+let varA = 'A' // B
+let varB = 'B' // C
+let varC = 'C' // A
+let temp;
+
+// temp = varA
+// varA = varB
+// varB = varC
+// varC = temp
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
---
+++ Seção 3 - JavaScript Básico/Aula 9 - Alert, Confirm e Prompt/exercício/script.js
@@ -0,0 +1,4 @@
+// Somar dois numeros atribuidos a duas variaveis inseridos pelo usuário
+let num1 = Number(prompt("Digite um número:"))
+let num2 = Number(prompt("Digite outro número:"))
+alert(`A soma de ${num1} e ${num2} é igual a ${num1 + num2}`)
