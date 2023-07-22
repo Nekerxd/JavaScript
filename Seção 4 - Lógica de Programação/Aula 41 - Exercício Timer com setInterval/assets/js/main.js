ed486e33b27e7b4a8496a3541d420b3ec0ae8587
Nekerxd <nuvielxd_01@hotmail.com>
2023-07-22 14:01:48 -0300

    Seção 4 - Refatoração Aula 41

--- Seção 4 - Lógica de Programação/Aula 41 - Exercício Timer com setInterval/assets/js/main.js
+++ Seção 4 - Lógica de Programação/Aula 41 - Exercício Timer com setInterval/assets/js/main.js
@@ -1,44 +1,52 @@
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+function main() {
 
-function calculaTempo(segundos) {
-    const data = new Date(segundos * 1000);
-
-    return data.toLocaleTimeString('pt-BR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZone: 'UTC',
-        hour12: false
+    const relogio = document.querySelector('.relogio');
+    const iniciar = document.querySelector('.iniciar');
+    const pausar = document.querySelector('.pausar');
+    const zerar = document.querySelector('.zerar');
+    
+    function calculaTempo(segundos) {
+        const data = new Date(segundos * 1000);
+    
+        return data.toLocaleTimeString('pt-BR', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            timeZone: 'UTC',
+            hour12: false
+        });
+    }
+    
+    let segundos = 0;
+    let tempo;
+    
+    function iniciaTempo() {
+        tempo = setInterval(function(){
+            segundos++;
+            relogio.innerHTML = calculaTempo(segundos);
+        }, 1000);
+    }
+    
+    document.addEventListener('click', function(event) {
+        const elemento = event.target;
+    
+        if (elemento.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(tempo);
+            iniciaTempo();
+        }
+    
+        if (elemento.classList.contains('pausar')) {
+            clearInterval(tempo);
+            relogio.classList.add('pausado');
+        }
+    
+        if (elemento.classList.contains('zerar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(tempo);
+            relogio.innerHTML = '00:00:00'
+            segundos = 0;
+        }
     });
 }
-
-let segundos = 0;
-let tempo;
-
-function iniciaTempo() {
-    tempo = setInterval(function(){
-        segundos++;
-        relogio.innerHTML = calculaTempo(segundos);
-    }, 1000);
-}
-
-iniciar.addEventListener('click', function() {
-    relogio.classList.remove('pausado');
-    clearInterval(tempo);
-    iniciaTempo();
-});
-
-pausar.addEventListener('click', function() {
-    clearInterval(tempo);
-    relogio.classList.add('pausado');
-});
-
-zerar.addEventListener('click', function(){
-    relogio.classList.remove('pausado');
-    clearInterval(tempo);
-    relogio.innerHTML = '00:00:00'
-    segundos = 0;
-});
+main();
